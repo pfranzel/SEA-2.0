b3b3240cf53d6a17a9d9f22a72e38a8b4a0f6ef7
pfranzel <80775775+pfranzel@users.noreply.github.com>
2021-05-19 14:17:28 +0200

    add missing files

---
+++ src/main/java/Salutations.java
@@ -0,0 +1,43 @@
+public enum Salutations {
+	MR, MRS, OTHER;
+
+	public static Salutations fromString(final String value) {
+
+		switch (value.toUpperCase()) {
+
+		case "MRS":
+		case "MRS.":
+		case "FEMALE":
+		case "F":
+		case "FRAU":
+			return MRS;
+		case "MR":
+		case "MR.":
+		case "MALE":
+		case "M":
+		case "MANN":
+			return MR;
+		case "OTHER":
+		case "DIVERSE":
+		case "D":
+			return OTHER;
+
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + value);
+		}
+	}
+
+	@Override
+	public String toString() {
+		switch (this) {
+		case MRS:
+			return "Mrs.";
+		case MR:
+			return "Mr.";
+		case OTHER:
+			return "Other";
+		default:
+			throw new IllegalArgumentException("Unexpected Case - please check...");
+		}
+	}
+}
---
+++ src/main/java/de/telekom/sea2/SeminarApp.java
@@ -0,0 +1,6 @@
+package de.telekom.sea2;
+
+class SeminarApp {
+	public void run(String[] args) {
+	}
+}
