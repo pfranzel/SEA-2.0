5f72ceb2291da2e04c13c6e691d3df1a3c35d13f
pfranzel <80775775+pfranzel@users.noreply.github.com>
2021-05-19 14:16:32 +0200

    General class/enum fillup/generation

--- src/main/java/de/telekom/sea2/Main.java
+++ src/main/java/de/telekom/sea2/Main.java
@@ -3,8 +3,7 @@ package de.telekom.sea2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SeminarApp seminarApp = new SeminarApp();
+		seminarApp.run(args);
 	}
-
 }
